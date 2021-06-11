48754260344539f8d9bf0d62c7b21ed3a17c4f33
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-06-11 11:34:30 +0600

    Add files via upload

---
+++ week show (1-7)/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+int week;
+printf("choice 1..7\n");
+scanf("%d",&week);
+
+switch(week)
+{
+case 1:
+printf("saterday");
+break;
+case 2:
+printf("sunday");
+break;
+case 3:
+printf("monday");
+break;
+case 4:
+printf("twesday");
+break;
+case 5:
+printf("wednusday");
+break;
+case 6:
+printf("thusday");
+break;
+case 7:
+printf("friday");
+break;
+
+}
+
+
+}
