fe3c005bd51cf30371f33af001c64a0f8854a680
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-06-11 11:33:39 +0600

    Add files via upload

---
+++ even and odd number/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a;
+   scanf(" %d", &a);
+   if(a%2==0)
+    printf("it is a even number",a);
+
+   else
+    printf(" it is a odd number",a);
+
+return 0;
+}
